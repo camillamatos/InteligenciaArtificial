69ba7e5012b8b0470dda366e812ddfe29beedee6
Camilla Matos <camilla@hygiabank.com>
2021-06-09 16:11:16 -0300

    algoritmo genetico

--- algoritmoGenetico/plane.py
+++ algoritmoGenetico/plane.py
@@ -1,16 +1,13 @@
 from random import Random
 from time import time
-from math import cos
-from math import pi
 from inspyred import ec
 from inspyred.ec import terminators
 import numpy as np
-import os
 
 #gerador de população
 def generate_(random, args):
     size = args.get('num_inputs', 12) 
-    return [random.randint(0, 18000) for i in range(size)] 
+    return [random.randint(0, 16000) for i in range(size)] 
 
 #função para avaliar soluções
 def evaluate_(candidates, args):
@@ -34,45 +31,59 @@ def perform_fitness(cs1, cs2, cs3, cs4, cs5, cs6, cs7, cs8, cs9, cs10, cs11, cs1
     cd4 = np.round(cs10)
     cc4 = np.round(cs11)
     ct4 = np.round(cs12)
-    fit = float((cd1 * 0.31 + cc1 * 0.31 + ct1 * 0.31 + cd2 * 0.38 + cc2 * 0.38 + ct2 * 0.38 + cd3 * 0.35 + cc3 * 0.35 + ct3 * 0.35 + cd4 * 0.285 + cc4 * 0.285 + ct4 * 0.285) / 12151.56)
-
+    fit = float(((cd1 * 0.31) + (cc1 * 0.31) + (ct1 * 0.31) + (cd2 * 0.38) + (cc2 * 0.38) + (ct2 * 0.38) + (cd3 * 0.35) + (cc3 * 0.35) + (ct3 * 0.35) + (cd4 * 0.285) + (cc4 * 0.285) + (ct4 * 0.285)) / 42565.92)
+  
     #restrições
+    cargaTotal = cd1 + cd2 + cd3 + cd4 + cc1 + cc2 + cc3 + cc4 + ct1 + ct2 + ct3 + ct4
+    num = 14
+
     #máximo de 10000kg na dianteira
-    h1 = np.maximum(0, float(((cd1 + cd2 + cd3 + cd4) - 10000) / (10000/10)))
+    h1 = np.maximum(0, float((cd1 + cd2 + cd3 + cd4) - 10000)) / (10000 / num)
 
     #máximo de 16000kg na central
-    h2 = np.maximum(0, float(((cc1 + cc2 + cc3 + cc4) - 16000) / (16000/10)))
+    h2 = np.maximum(0, float((cc1 + cc2 + cc3 + cc4) - 16000)) / (16000 / num)
 
     #máximo de 8000kg na traseira
-    h3 = np.maximum(0, float(((ct1 + ct2 + ct3 + ct4) - 8000) / (8000/10)))
+    h3 = np.maximum(0, float((ct1 + ct2 + ct3 + ct4) - 8000)) / ( 8000 / num)
 
     #máximo de 18000kg da carga 1
-    h4 = np.maximum(0, float(((cd1 + cc1 + ct1) - (18000)) / (18000/10)))
+    h4 = np.maximum(0, float((cd1 + cc1 + ct1) - 18000))/ (18000 / num)
 
     #máximo de 15000kg da carga 2
-    h5 = np.maximum(0, float(((cd2 + cc2 + ct2) - (15000)) / (15000/10)))
+    h5 = np.maximum(0, float((cd2 + cc2 + ct2) - 15000)) / (15000 / num)
 
     #máximo de 23000kg da carga 3
-    h6 = np.maximum(0, float(((cd3 + cc3 + ct3) - (23000)) / (23000/10)))
+    h6 = np.maximum(0, float((cd3 + cc3 + ct3) - 23000)) / (23000 / num)
 
     #máximo de 12000kg da carga 4
-    h7 = np.maximum(0, float(((cd4 + cc4 + ct4) - (12000)) / (12000/10)))
+    h7 = np.maximum(0, float((cd4 + cc4 + ct4) - 12000))/ (12000 / num)
 
     #máximo de 6800m³ na dianteira
-    h8 = np.maximum(0, float(((cd1 * 0.48 + cd2 * 0.65 + cd3 * 0.58 + cd4 * 0.39) - 6800) / (6800/10)))
+    h8 = np.maximum(0, float(((cd1 * 0.48) + (cd2 * 0.65) + (cd3 * 0.58) + (cd4 * 0.39)) - 6800)) / (6800 / num )
 
     #máximo de 8700m³ na central
-    h9 = np.maximum(0, float(((cc1 * 0.48 + cc2 * 0.65 + cc3 * 0.58 + cc4 * 0.39) - 8700) / (8700/10)))
+    h9 = np.maximum(0, float(((cc1 * 0.48) + (cc2 * 0.65) + (cc3 * 0.58) + (cc4 * 0.39)) - 8700)) / (8700 / num)
 
     #máximo de 5300m³ na traseira
-    h10 = np.maximum(0, float(((ct1 * 0.48 + ct2 * 0.65 + ct3 * 0.58 + ct4 * 0.39) - 5300) / (5300/10)))
+    h10 = np.maximum(0, float(((ct1 * 0.48) + (ct2 * 0.65) + (ct3 * 0.58) + (ct4 * 0.39)) - 5300)) / (5300 / num)
 
-    fit = fit - (h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9 + h10)
+    #proporção da carga dianteira
+    h11 = np.maximum(0, float((((cd1 + cd2 + cd3 + cd4) / cargaTotal) - 0.29411764706))) / (0.29411764706 / num)
+
+    #proporção da carga central
+    h12 = np.maximum(0, float((((cc1 + cc2 + cc3 + cc4) / cargaTotal) - 0.47058823529))) / (0.47058823529 / num)
+    
+    #proporção da carga traseira
+    h13 = np.maximum(0, float((((ct1 + ct2 + ct3 + ct4) / cargaTotal) - 0.23529411765))) / (0.23529411765 / num)
+
+    #peso total no avião
+    h14 = np.maximum(0, float(cargaTotal - 34000)) / (34000 / num)
+
+    fit = fit - (h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9 + h10 + h11 + h12 + h13 + h14)
     return fit
 
 
 def solution_evaluation(CD1, CC1, CT1, CD2, CC2, CT2, CD3, CC3, CT3, CD4, CC4, CT4):
-
     CD1 = np.round(CD1)
     CD2 = np.round(CD2)
     CD3 = np.round(CD3)
@@ -86,43 +97,51 @@ def solution_evaluation(CD1, CC1, CT1, CD2, CC2, CT2, CD3, CC3, CT3, CD4, CC4, C
     CC3 = np.round(CC3)
     CC4 = np.round(CC4)
 
+    volumeDianteiro = float((CD1*0.48)+(CD2*0.65)+(CD3*0.58)+(CD4*0.39))
+    volumeCentral = float((CC1*0.48)+(CC2*0.65)+(CC3*0.58)+(CC4*0.39))
+    volumeTraseiro = float((CT1*0.48)+(CT2*0.65)+(CT3*0.58)+(CT4*0.39))
+    pesoDianteiro = float(CD1 + CD2 + CD3 + CD4)
+    pesoCentral = float(CC1 + CC2 + CC3 + CC4)
+    pesoTraseiro = float(CT1 + CT2 + CT3 + CT4)
+    pesoTotal = pesoDianteiro + pesoCentral + pesoTraseiro
+
+    lucro1 = float((CD1*0.31)+(CC1*0.31)+(CT1*0.31))
+    lucro2 = float((CD2*0.38)+(CC2*0.38)+(CT2*0.38))
+    lucro3 = float((CD3*0.35)+(CC3*0.35)+(CT3*0.35))
+    lucro4 = float((CD4*0.285)+(CC4*0.285)+(CT4*0.285))
+
     print("")
     print("RESUMO DA EXECUÇÃO")
     print("")
-    print("Lucro total: ", float((CD1*0.31)+(CC1*0.31)+(CT1*0.31)+(CD2*0.38)+(CC2*0.38)+(CT2*0.38)+(CD3*0.35)+(CC3*0.35)+(CT3*0.35)+(CD4*0.285)+(CC4*0.285)+(CT4*0.285)))
-    print("Volume no compartimento dianteiro", float((CD1*0.48)+(CD2*0.65)+(CD3*0.58)+(CD4*0.39)))
-    print("Volume no compartimento central",float((CC1*0.48)+(CC2*0.65)+(CC3*0.58)+(CC4*0.39)))
-    print("Volume no compartimento traseiro",float((CT1*0.48)+(CT2*0.65)+(CT3*0.58)+(CT4*0.39)))
-    print("Total volumétrico: ",float((CD1*0.48)+(CD2*0.65)+(CD3*0.58)+(CD4*0.39)+(CC1*0.48)+(CC2*0.65)+(CC3*0.58)+(CC4*0.39)+(CT1*0.48)+(CT2*0.65)+(CT3*0.58)+(CT4*0.39)))
-    print("")
-    print("Peso no compartimento dianteiro", float(CD1 + CD2 + CD3 + CD4))
-    print("Peso no compartimento central", float(CC1 + CC2 + CC3 + CC4))
-    print("Peso no compartimento traseiro", float(CT1 + CT2 + CT3 + CT4))
-    print("Total do peso: ",float(CD1 + CD2 + CD3 + CD4 + CC1 + CC2 + CC3 + CC4 + CT1 + CT2 + CT3 + CT4))
+    print("Lucro total: ", (lucro1 + lucro2 + lucro3 + lucro4))
+    print("Compartimento dianteiro: Peso = ", pesoDianteiro, "kg - ", (pesoDianteiro/pesoTotal)*100, '%  |  Volume = ', volumeDianteiro)
+    print("Compartimento central: Peso = ", pesoCentral, "kg - ", (pesoCentral/pesoTotal)*100, '%  |  Volume = ', volumeCentral)
+    print("Compartimento traseiro: Peso = ", pesoTraseiro, "kg - ", (pesoTraseiro/pesoTotal)*100, '%  |  Volume = ', volumeTraseiro)
+    print("Total de peso: ", pesoTotal, "kg | Total volumétrico: ", (volumeDianteiro + volumeCentral + volumeTraseiro))
     print("")
     print("Carga 1 no compartimento dianteiro: ", CD1)
     print("Carga 1 no compartimento central: ", CC1)
     print("Carga 1 no compartimento traseiro: ", CT1)
     print("Total da carga 1: ", CD1 + CC1 + CT1)
-    print("Lucro carga 1: ", float((CD1*0.31)+(CC1*0.31)+(CT1*0.31)))
+    print("Lucro carga 1: ", lucro1)
     print("")
     print("Carga 2 no compartimento dianteiro: ", CD2)
     print("Carga 2 no compartimento central: ", CC2)
     print("Carga 2 no compartimento traseiro: ", CT2)
     print("Total da carga 2: ", CD2 + CC2 + CT2)
-    print("Lucro carga 2: ", float((CD2*0.38)+(CC2*0.38)+(CT2*0.38)))
+    print("Lucro carga 2: ", lucro2)
     print("")
     print("Carga 3 no compartimento dianteiro: ", CD3)
     print("Carga 3 no compartimento central: ", CC3)
     print("Carga 3 no compartimento traseiro: ", CT3)
     print("Total da carga 3: ", CD3 + CC3 + CT3)
-    print("Lucro carga 3: ", float((CD3*0.35)+(CC3*0.35)+(CT3*0.35)))
+    print("Lucro carga 3: ", lucro3)
     print("")
     print("Carga 4 no compartimento dianteiro: ", CD4)
     print("Carga 4 no compartimento central: ", CC4)
     print("Carga 4 no compartimento traseiro: ", CT4)
     print("Total da carga 4: ", CD4 + CC4 + CT4)
-    print("Lucro carga 4: ", float((CD4*0.285)+(CC4*0.285)+(CT4*0.285)))
+    print("Lucro carga 4: ", lucro4)
 
 def main():
     rand = Random()
@@ -143,12 +162,12 @@ def main():
                           evaluator=evaluate_,
                           pop_size=10000,
                           maximize=True,
-                          bounder=ec.Bounder(0, 18000),
-                          max_generations=1000,
+                          bounder=ec.Bounder(0, 16000),
+                          max_generations=2000,
                           num_inputs= 12,
                           crossover_rae = 1.0,
                           num_crossover_points = 1,
-                          mutation_rate = 0.5,
+                          mutation_rate = 0.6,
                           num_elites = 1,
                           num_selected = 12,
                           tournament_size = 12,
